efc84f694333d452fdeea785ef9e49d67f12b47e
Bruno <brunohenriquehns@gmail.com>
2025-09-04 11:44:06 -0300

    add resolution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "IMEI123456789", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 13", "IMEI987654321", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
